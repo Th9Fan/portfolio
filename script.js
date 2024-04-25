f4676bff846ba3e08cf6f45fe47a840ee4c54558
Th9Fan <theofan@ivkin.net>
2024-04-24 17:37:23 -0700

    done slider

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
+
+    // direction: '',
     loop: true,
+    effect: 'coverflow',
+    depth:100,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
   });
+
