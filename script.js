2b1690fb353ab3bb27bf3f7eb16c1d9148ac80fa
Th9Fan <theofan@ivkin.net>
2024-05-01 17:05:21 -0700

    onclic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const swiper = new Swiper('.swiper', {
 
     // direction: '',
@@ -7,3 +8,21 @@ const swiper = new Swiper('.swiper', {
   
   });
 
+const menu = document.querySelector(".menu");
+const menu_link = document.querySelector(".menu-link");
+
+function changeactive (event){
+  console.log(event.target)
+  const target = event.target;
+  if (target.classList.contains("menu-link")){
+    console.log("button was klicked!")
+            
+    activeBtn.classList.remove("active");
+    arget.classList.add("active");
+    activeBtn = target
+    uptate()
+  }
+  menu.onclick=function
+}
+
+menu_link.onclick= changeactive
