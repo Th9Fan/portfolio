491575615319d6faf9d90f2b617fe011c81d5ffa
Th9Fan <theofan@ivkin.net>
2024-05-01 17:47:01 -0700

    schrooll

--- script.js
+++ script.js
@@ -9,20 +9,40 @@ const swiper = new Swiper('.swiper', {
   });
 
 const menu = document.querySelector(".menu");
-const menu_link = document.querySelector(".menu-link");
+const menu_link = menu.querySelectorAll(".menu-link");
 
 function changeactive (event){
   console.log(event.target)
-  const target = event.target;
-  if (target.classList.contains("menu-link")){
-    console.log("button was klicked!")
-            
-    activeBtn.classList.remove("active");
-    arget.classList.add("active");
-    activeBtn = target
-    uptate()
+  const target = event.target
+  if (target.classList.contains('menu-link')){
+    const preactive = menu.querySelector(".active");
+    preactive.classList.remove("active")
+    target.classList.add("active")
   }
-  menu.onclick=function
 }
 
-menu_link.onclick= changeactive
+menu.onclick= changeactive
+
+window.onscroll = () => {
+  const h = document.documentElement.clientHeight;
+  let newactiv = null;
+  if (window.scrollY >= h  * 4){
+    newactiv = menu_link[4];
+  }
+  else if (window.scrollY >= h  * 3){
+    newactiv = menu_link[3];
+  }
+  else if (window.scrollY >= h  * 2){
+    newactiv = menu_link[2];
+  }
+  else if (window.scrollY >= h  ){
+    newactiv = menu_link[1];
+  }
+  else{
+    newactiv = menu_link[0];
+  }
+  const preactive = menu.querySelector(".active");
+  preactive.classList.remove("active")
+  newactiv.classList.add("active")
+
+}
