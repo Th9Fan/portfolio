248b8b0c76f62a90a48e8fe16d7388f51be9e86f
Th9Fan <theofan@ivkin.net>
2024-05-08 17:20:36 -0700

    mobiel menu v3

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const swiper = new Swiper('.swiper', {
 const menu = document.querySelector(".menu");
 const menu_link = menu.querySelectorAll(".menu-link");
 const mobimenu = document.querySelector(".mobile-menu")
+const mobi_link = mobimenu.querySelector(".menu-link")
 
 function changeactive (event){
   console.log(event.target)
@@ -23,41 +24,38 @@ function changeactive (event){
   }
 }
 
-function changeactive2 (event){
-  console.log(event.target)
-  const target = event.target
-  
-  if (target.classList.contains('mobile-menu')){
-    const preactive = mobimenu.querySelector(".active");
-    preactive.classList.remove("active")
-    target.classList.add("active")
-  }
-}
-
-mobimenu.onclick= changeactive2
 menu.onclick= changeactive
 
 window.onscroll = () => {
   const h = document.documentElement.clientHeight;
   let newactiv = null;
+  let newmobiel = null;
   if (window.scrollY >= h  * 4){
     newactiv = menu_link[4];
+    newmobiel = mobi_link[4];
   }
   else if (window.scrollY >= h  * 3){
     newactiv = menu_link[3];
+    newmobiel = mobi_link[3];
   }
   else if (window.scrollY >= h  * 2){
     newactiv = menu_link[2];
+    newmobiel = mobi_link[2];
   }
   else if (window.scrollY >= h  ){
     newactiv = menu_link[1];
+    newmobiel = mobi_link[1];
   }
   else{
     newactiv = menu_link[0];
+    newmobiel = mobi_link[0];
   }
   const preactive = menu.querySelector(".active");
   preactive.classList.remove("active")
   newactiv.classList.add("active")
+  const premobiel = mobimenu.querySelector(".active");
+  premobiel.classList.remove("active")
+  newmobiel.classList.add("active")
 
 }
 
